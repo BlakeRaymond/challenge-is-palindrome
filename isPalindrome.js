89c7c5326432f64bccc4359d8b7c81e99b588e4b
Blake Raymond <blake.crypto@technologist.com>
2023-04-24 10:32:23 -0600

    palindrome

---
+++ isPalindrome.js
@@ -0,0 +1,16 @@
+// Write your code below
+
+// Check to see if we write the word forward and then write it back word if it is the same
+
+function isPalindrome(word) {
+    let splitWord = word.split("");
+    let reverseArray = splitWord.reverse();
+    let joinArr = reverseArray.join(""); 
+    if (word === joinArr) {
+        return 'is palindrome'
+    }
+    else {
+        return 'is not palindrome'
+    }
+}
+console.log(isPalindrome('racecar'))
